73233b959d1d3e3f1ae9f2a68e9ac668d82d8272
Stephen Hurley <stephenhurley31@gmail.com>
2021-03-19 22:16:50 +0000

    file structure and skeleton of project

---
+++ scripts/gameScript.js
@@ -0,0 +1,17 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+
+function headClicked()
+{
+	alert("head button worked");
+}
+
+function bodyClicked()
+{
+	alert("body button worked");
+}
+
+function legsClicked()
+{
+	alert("legs button worked");
+}
