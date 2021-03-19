299f1e6357e2082683a5f4e24b4b0f495f1cdee5
Stephen Hurley <stephenhurley31@gmail.com>
2021-03-19 22:55:29 +0000

    entering name through form

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -1,6 +1,15 @@
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
 
+function splitFunction() 
+{
+	console.log("split function called")
+	var url = window.location.search;
+	console.log(url);
+	var result = url.split("="); // Splits string based on =
+	document.getElementById("playerName").innerHTML = result[1];
+}
+
 function headClicked()
 {
 	alert("head button worked");
